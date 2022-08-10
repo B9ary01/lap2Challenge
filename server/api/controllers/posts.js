d03abb2cb6138547d19e76269aeb72f330ccad5d
B9ary01 <bishalrai111@yahoo.com>
2022-08-10 12:24:19 +0100

    fix post route

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -14,8 +14,8 @@ async function index (req, res) {
 
 async function create (req, res) {
     try {
-        const post = await Post.create(req.body);
-        res.status(200).json(post);
+        const post = await Post.create(req.body.title, req.body.name, req.body.post);
+        res.status(201).json(post);
     } catch (err) {
         res.status(422).json({err})
     }
