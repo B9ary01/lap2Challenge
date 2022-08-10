0871abb867baebd73e44ef85aedad3e24e9bd7ed
B9ary01 <bishalrai111@yahoo.com>
2022-08-10 10:46:31 +0100

    update server file

--- server/api/server.js
+++ server/api/server.js
@@ -6,7 +6,9 @@ server.use(cors());
 server.use(express.json());
 
 
+const postsRoutes = require('./routes/posts');
 
+server.use('/posts', postsRoutes);
 
 // Root route
 server.get('/', (req, res) => res.send(' Welcome to the Post App!'))
